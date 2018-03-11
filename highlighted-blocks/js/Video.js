c28f67ff0862b008b56c54b14e85f6d210e2533b
serp-ya <sergei.kundryukov1989@gmail.com>
2018-03-11 20:43:12 +0300

    Формат код-стайла

--- highlighted-blocks/js/Video.js
+++ highlighted-blocks/js/Video.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const Video = props => {
-    return (
-        <div className="item item-video">
-            <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            <p className="views">Просмотров: {props.views}</p>
-        </div>
-    )
+  return (
+    <div className="item item-video">
+      <iframe src={props.url} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  )
 };
