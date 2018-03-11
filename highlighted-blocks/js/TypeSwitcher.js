8fb0b1ba0e69dcc19c83fcd250c9516d624e03f5
serp-ya <sergei.kundryukov1989@gmail.com>
2018-03-11 22:41:25 +0300

    Добавлен компонент переключения типов

---
+++ highlighted-blocks/js/TypeSwitcher.js
@@ -0,0 +1,17 @@
+const TypeSwitcher = ({ item }) => {
+  switch (item.type) {
+    case 'video':
+      return (
+        <PopularitySwitcher item = {item}>
+          {Video}
+        </PopularitySwitcher>
+      );
+
+    case 'article':
+      return (
+        <PopularitySwitcher item = {item}>
+          {Article}
+        </PopularitySwitcher>
+      );
+  }
+};
